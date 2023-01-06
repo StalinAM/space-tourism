f466ba98a4ca0fbca7121c9146e607d698666beb
Stalin Acurio <sta_vla411@hotmail.com>
2023-01-06 11:23:19 -0500

    import font

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import { Header } from '../components/Header'
+import { Barlow_Condensed } from '@next/font/google'
+
+const fontBarlow = Barlow_Condensed({
+  subsets: ['latin'],
+  weight: ['400', '600']
+})
+
 export default function RootLayout({ children }) {
   return (
-    <html>
+    <html className={fontBarlow.className}>
       <head />
       <body>
         <Header />
