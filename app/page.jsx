7ec0378b89f16bb0f86478c85f25de6db16dd0cc
Stalin Acurio <sta_vla411@hotmail.com>
2023-01-07 15:56:20 -0500

    add css paramater and finished

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from 'next/link'
 import styles from './Home.module.css'
 import { Bellefair } from '@next/font/google'
@@ -6,12 +7,13 @@ const fontBellefair = Bellefair({
   weight: ['400']
 })
 export default function Page() {
+ 
   return (
     <main className={styles.container}>
       <div className={styles.background}></div>
       <div>
-        <h2>So, you want to travel to</h2>
-        <h1>Space</h1>
+        <h2 className={fontBellefair.className}>So, you want to travel to</h2>
+        <h1 className={fontBellefair.className}>Space</h1>
         <p>
           Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
