b968a81eeac6524faca887cddd0123881b143308
Stalin Acurio <sta_vla411@hotmail.com>
2023-01-07 20:31:18 -0500

    add line in nav planets

--- app/destination/layout.jsx
+++ app/destination/layout.jsx
@@ -1,20 +1,29 @@
+'use client'
 import data from '../../data/data.json'
 import styles from './Destination.module.css'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 export default function DestinationLayout({ children }) {
+  const pathname = usePathname()
   return (
     <main className={styles.container}>
       <div className={styles.background}></div>
       <h2>
         <span>01</span>PICK YOUR DESTINATION
       </h2>
-      <ul>
+      <ul className={styles.container_nav}>
         {data.destinations.map((item, index) => (
           <li key={item.id}>
-            <span>0{index + 1}</span>
             <Link href={`/destination/${item.id}`}>
               {item.name.toUpperCase()}
             </Link>
+            <div
+              className={`${styles.line} ${
+                pathname == item.id || pathname.includes(item.id)
+                  ? styles.opacity
+                  : ''
+              }`}
+            ></div>
           </li>
         ))}
       </ul>
