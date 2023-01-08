bfb45c1da8dc5afe8f521cbd78f8aeaeffb6c7f0
Stalin Acurio <sta_vla411@hotmail.com>
2023-01-08 18:19:55 -0500

    add font

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,25 @@
 'use client'
 import Link from 'next/link'
 import styles from './Home.module.css'
-import { Bellefair } from '@next/font/google'
+import { Bellefair, Barlow } from '@next/font/google'
+
 const fontBellefair = Bellefair({
   subsets: ['latin'],
   weight: ['400']
 })
+const fontBarlow = Barlow({
+  subsets: ['latin'],
+  weight: ['400']
+})
+
 export default function Page() {
- 
   return (
-    <main className={styles.container}>
+    <main className={`${styles.container} ${fontBellefair.className}`}>
       <div className={styles.background}></div>
       <div>
-        <h2 className={fontBellefair.className}>So, you want to travel to</h2>
-        <h1 className={fontBellefair.className}>Space</h1>
-        <p>
+        <h2>So, you want to travel to</h2>
+        <h1>Space</h1>
+        <p className={fontBarlow.className}>
           Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
           back, and relax because we’ll give you a truly out of this world
