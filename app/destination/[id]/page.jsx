80ec21b2694262c07bb735ed4aa7ffe85bc653fe
Stalin Acurio <sta_vla411@hotmail.com>
2023-01-08 10:10:05 -0500

    refactor

--- app/destination/[id]/page.jsx
+++ app/destination/[id]/page.jsx
@@ -1,5 +1,6 @@
 import { PlanetsInfo } from '../../../components/PlanetsInfo'
 import data from '../../../data/data.json'
+
 export default function Planets({ params }) {
   const { id } = params
   const objId = data.destinations.find((item) => item.id == id)
