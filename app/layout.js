b22a382382c1e5ca5f5d5a9ace324f6d1da1ba23
Stalin Acurio <sta_vla411@hotmail.com>
2023-01-09 01:14:16 -0500

    add font Barlow

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,9 @@
 import '../styles/globals.css'
 import { Header } from '../components/Header'
-import { Barlow_Condensed } from '@next/font/google'
-
-const fontBarlow = Barlow_Condensed({
-  subsets: ['latin'],
-  weight: ['400', '600']
-})
 
 export default function RootLayout({ children }) {
   return (
-    <html className={fontBarlow.className}>
+    <html>
       <head />
       <body>
         <Header />
